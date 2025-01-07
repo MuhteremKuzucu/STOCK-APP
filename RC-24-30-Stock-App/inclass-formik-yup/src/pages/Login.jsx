528c5d2bd21c1fd064b290b4d81618a3e7b26a37
MuhteremKuzucu <kuzucumuhterem@gmail.com>
2025-01-07 17:30:41 +0100

    login

--- RC-24-30-Stock-App/inclass-formik-yup/src/pages/Login.jsx
+++ RC-24-30-Stock-App/inclass-formik-yup/src/pages/Login.jsx
@@ -9,9 +9,20 @@ import image from "../assets/hero.png";
 import { Link } from "react-router-dom";
 import AuthHeader from "../components/AuthHeader";
 import AuthImage from "../components/AuthImage";
+import { Formik } from "formik";
+import { TextField, Button } from "@mui/material";
+import * as Yup from "yup";
 
 const Login = () => {
   const theme = useTheme();
+
+  const SignupSchema = Yup.object().shape({
+    email: Yup.string()
+      .email("Geçerli bir Email giriniz")
+      .required("Email zorunludur"),
+    password: Yup.string()
+      .required("Password zorunludur")
+  });
 
   return (
     <Container maxWidth="lg">
@@ -41,10 +52,65 @@ const Login = () => {
             SIGN IN
           </Typography>
 
+          <Formik
+            initialValues={{
+              email: "",
+              password: "",
+            }}
+            validationSchema={SignupSchema}
+            onSubmit={(values) => {}}
+          >
+            {({
+              values,
+              errors,
+              touched,
+              handleChange,
+              handleBlur,
+              handleSubmit,
+              isSubmitting,
+            }) => (
+              <form onSubmit={handleSubmit}>
+                <TextField
+                  name="email"
+                  value={values.email}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  error={touched.email && errors.email}
+                  helperText={touched.email && errors.email}
+                  label="Email"
+                  variant="outlined"
+                  fullWidth
+                  type="email"
+                  margin="normal"
+                />
+                <TextField
+                  name="password"
+                  value={values.password}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  error={touched.password && errors.password}
+                  helperText={touched.password && errors.password}
+                  label="Password"
+                  variant="outlined"
+                  fullWidth
+                  type="password"
+                  margin="normal"
+                />
+
+                <Button
+                  type="submit"
+                  variant="contained"
+                  fullWidth
+                  margin="normal"
+                >
+                  SUBMIT
+                </Button>
+              </form>
+            )}
+          </Formik>
+
           <Box sx={{ textAlign: "center", mt: 2, color: "secondary.main" }}>
-            <Link to="/register">
-              Don't have an account? Sign Up
-            </Link>
+            <Link to="/register">Don't have an account? Sign Up</Link>
           </Box>
         </Grid>
 
